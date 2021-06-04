9876588a7376407e1e54c811e17adbc885366284
qinjie10 <qinjie10@meituan.com>
2021-06-04 13:36:58 +0800

    第二次提交代码

--- src/com/company/NewCode.java
+++ src/com/company/NewCode.java
@@ -3,6 +3,7 @@ package com.company;
 public class NewCode {
     public static void main(String[] args) {
         //记录算法记录
-
+        System.out.println("第一个次修改代码");
+        System.out.println("第二个次修改代码");
     }
 }
